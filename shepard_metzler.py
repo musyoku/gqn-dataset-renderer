e62cdecda561a82a4e22bd6903b97baa77ac09e0
musyoku <musyoku@users.noreply.github.com>
2019-03-13 19:03:57 +0900

    Improve code

--- shepard_metzler.py
+++ shepard_metzler.py
@@ -11,8 +11,9 @@ import numpy as np
 import pyglet
 import trimesh
 from pyrender import (DirectionalLight, Mesh, MetallicRoughnessMaterial, Node,
-                      OffscreenRenderer, PerspectiveCamera, PointLight,
-                      Primitive, Scene, SpotLight, Viewer)
+                      OffscreenRenderer, PerspectiveCamera, OrthographicCamera,
+                      PointLight, Primitive, Scene, SpotLight, Viewer,
+                      RenderFlags)
 from tqdm import tqdm
 
 import gqn
@@ -103,9 +104,9 @@ def build_scene(color_array):
     # Place cubes
     scene = Scene(
         bg_color=np.array([0.0, 0.0, 0.0]),
-        ambient_light=np.array([0.2, 0.2, 0.2, 1.0]))
+        ambient_light=np.array([0.3, 0.3, 0.3, 1.0]))
     for position in cube_position_array:
-        boxv_trimesh = trimesh.creation.box(extents=1 * np.ones(3))
+        boxv_trimesh = trimesh.creation.box(extents=np.ones(3))
         color = np.array(random.choice(color_array))
         vertex_colors = np.broadcast_to(color, boxv_trimesh.vertices.shape)
         boxv_trimesh.visual.vertex_colors = vertex_colors
@@ -120,12 +121,35 @@ def build_scene(color_array):
             ])))
         scene.add_node(node)
 
-    light = PointLight(color=np.ones(3), intensity=0.0)
-    light = DirectionalLight(color=np.ones(3), intensity=10.0)
-    node = Node(light=light, translation=np.array([0, 5, 5]))
+    # Place a light
+    light = DirectionalLight(color=np.ones(3), intensity=20.0)
+    quaternion_yaw = generate_quaternion(yaw=(math.pi / 4))
+    quaternion_pitch = generate_quaternion(pitch=(-math.pi / 5))
+    quaternion = multiply_quaternion(quaternion_pitch, quaternion_yaw)
+    quaternion = quaternion / np.linalg.norm(quaternion)
+    node = Node(
+        light=light, rotation=quaternion, translation=np.array([1, 1, 1]))
     scene.add_node(node)
 
     return scene
+
+
+def generate_quaternion(yaw=None, pitch=None):
+    if yaw is not None:
+        return np.array([
+            0,
+            math.sin(yaw / 2),
+            0,
+            math.cos(yaw / 2),
+        ])
+    if pitch is not None:
+        return np.array([
+            math.sin(pitch / 2),
+            0,
+            0,
+            math.cos(pitch / 2),
+        ])
+    raise NotImplementedError
 
 
 def multiply_quaternion(A, B):
@@ -133,7 +157,7 @@ def multiply_quaternion(A, B):
     b = B[3]
     U = A[:3]
     V = B[:3]
-    W = a * V + b * U + U * V
+    W = a * V + b * U + np.cross(V, U)
     return np.array([W[0], W[1], W[2], a * b - U @ V])
 
 
@@ -141,96 +165,83 @@ def main():
     # Initialize colors
     color_array = []
     for n in range(args.num_colors):
-        hue = n / (args.num_colors - 1)
-        saturation = 0.9
+        hue = n / args.num_colors
+        saturation = 1
         lightness = 1
         red, green, blue = colorsys.hsv_to_rgb(hue, saturation, lightness)
         color_array.append((red, green, blue))
 
-    screen_width = args.image_size
-    screen_height = args.image_size
-
     scene = build_scene(color_array)
-    camera = PerspectiveCamera(yfov=(np.pi / 3.0))
-    camera_node = Node(
-        camera=camera,
-        rotation=np.array([0,
-                           math.sin(math.pi / 2), 0,
-                           math.cos(math.pi / 2)]),
-        translation=np.array([0, 0, -5]))
+    camera = OrthographicCamera(xmag=3.5, ymag=3.5)
+    camera_node = Node(camera=camera)
     scene.add_node(camera_node)
-    r = OffscreenRenderer(viewport_width=64, viewport_height=64)
-    start_time = time.time()
+    renderer = OffscreenRenderer(
+        viewport_width=args.image_size, viewport_height=args.image_size)
+
+    camera_distance = 5
+
+    # for k in range(1000):
+    #     yaw = math.pi * 2 * k / 100
+    #     pitch = -math.atan(3 / 5)
+    #     quaternion_yaw = generate_quaternion(yaw=yaw)
+    #     quaternion_pitch = generate_quaternion(pitch=pitch)
+    #     quaternion = multiply_quaternion(quaternion_pitch, quaternion_yaw)
+    #     quaternion = quaternion / np.linalg.norm(quaternion)
+    #     camera_node.rotation = quaternion
+    #     camera_node.translation = np.array(
+    #         [5 * math.sin(yaw), 3, 5 * math.cos(yaw)])
+    #     color, depth = renderer.render(scene, flags=RenderFlags.SHADOWS_DIRECTIONAL)
+    #     plt.clf()
+    #     plt.imshow(color)
+    #     plt.pause(1e-10)
+    # renderer.delete()
+
+
+    # for k in range(1000):
+    #     x = math.sin(math.pi * 2 * k / 1000)
+    #     z = math.cos(math.pi * 2 * k / 1000)
+    #     if z < 0:
+    #         yaw = math.pi + math.atan(x / z)
+    #     elif x < 0:
+    #         yaw = math.pi * 2 + math.atan(x / z)
+    #     else:
+    #         yaw = math.atan(x / z)
+    #     print(x, z, yaw)
+    # exit()
+
     for k in range(1000):
-        yaw = math.pi * 2 * k / 100
-        pitch = -math.pi / 4
-        quaternion_yaw = np.array([
-            0,
-            math.sin(yaw / 2),
-            0,
-            math.cos(yaw / 2),
-        ])
-        quaternion_pitch = np.array([
-            math.sin(pitch / 2),
-            0,
-            0,
-            math.cos(pitch / 2),
-        ])
+        camera_position = np.random.normal(size=3)
+        camera_position = camera_distance * camera_position / np.linalg.norm(
+            camera_position)
+        print(camera_position)
+
+        x, y, z = camera_position
+        if z < 0:
+            yaw = math.pi + math.atan(x / z)
+        elif x < 0:
+            yaw = math.pi * 2 + math.atan(x / z)
+        else:
+            yaw = math.atan(x / z)
+        pitch = -math.asin(y / camera_distance)
+
+        print(yaw, pitch)
+
+        # camera_position = np.array([0, 0, camera_distance])
+        # yaw = math.pi * k / 1000
+        # pitch = 0
+
+        quaternion_yaw = generate_quaternion(yaw=yaw)
+        quaternion_pitch = generate_quaternion(pitch=pitch)
         quaternion = multiply_quaternion(quaternion_pitch, quaternion_yaw)
         quaternion = quaternion / np.linalg.norm(quaternion)
         camera_node.rotation = quaternion
-        camera_node.translation = np.array(
-            [5 * math.sin(yaw), 3, 5 * math.cos(yaw)])
-        color, depth = r.render(scene)
+        camera_node.translation = camera_position
+        color, depth = renderer.render(
+            scene, flags=RenderFlags.SHADOWS_DIRECTIONAL)
         plt.clf()
         plt.imshow(color)
-        plt.pause(1e-10)
-    r.delete()
-
-    renderer = rtx.Renderer()
-    render_buffer = np.zeros(
-        (screen_height, screen_width, 3), dtype=np.float32)
-
-    dataset = gqn.archiver.Archiver(
-        directory=args.output_directory,
-        total_observations=args.total_observations,
-        num_observations_per_file=min(args.num_observations_per_file,
-                                      args.total_observations),
-        image_size=(args.image_size, args.image_size),
-        num_views_per_scene=args.num_views_per_scene,
-        initial_file_number=args.initial_file_number)
-
-    camera = rtx.OrthographicCamera()
-
-    for _ in tqdm(range(args.total_observations)):
-        scene = build_scene(color_array)
-        scene_data = gqn.archiver.SceneData((args.image_size, args.image_size),
-                                            args.num_views_per_scene)
-
-        view_radius = 3.3
-
-        for _ in range(args.num_views_per_scene):
-            eye = np.random.normal(size=3)
-            eye = tuple(view_radius * (eye / np.linalg.norm(eye)))
-            center = (0, 0, 0)
-            camera.look_at(eye, center, up=(0, 1, 0))
-
-            renderer.render(scene, camera, rt_args, cuda_args, render_buffer)
-
-            # Convert to sRGB
-            image = np.power(np.clip(render_buffer, 0, 1), 1.0 / 2.2)
-            image = np.uint8(image * 255)
-            image = cv2.bilateralFilter(image, 3, 25, 25)
-
-            # plt.imshow(image, interpolation="none")
-            # plt.pause(1e-8)
-
-            yaw = gqn.math.yaw(eye, center)
-            pitch = gqn.math.pitch(eye, center)
-            scene_data.add(image, eye, math.cos(yaw), math.sin(yaw),
-                           math.cos(pitch), math.sin(pitch))
-
-        dataset.add(scene_data)
+        plt.pause(1)
+    renderer.delete()
 
 
 if __name__ == "__main__":
@@ -244,7 +255,7 @@ if __name__ == "__main__":
     parser.add_argument("--num-views-per-scene", "-k", type=int, default=15)
     parser.add_argument("--image-size", type=int, default=64)
     parser.add_argument("--num-cubes", "-cubes", type=int, default=5)
-    parser.add_argument("--num-colors", "-colors", type=int, default=12)
+    parser.add_argument("--num-colors", "-colors", type=int, default=10)
     parser.add_argument(
         "--output-directory",
         "-out",
@@ -252,69 +263,3 @@ if __name__ == "__main__":
         default="dataset_shepard_matzler_train")
     args = parser.parse_args()
     main()
-
-boxv_trimesh = trimesh.creation.box(extents=0.1 * np.ones(3))
-boxv_vertex_colors = np.zeros((boxv_trimesh.vertices.shape), dtype=np.float32)
-boxv_vertex_colors[:, 0] = 1.0
-boxv_trimesh.visual.vertex_colors = boxv_vertex_colors
-boxv_mesh = Mesh.from_trimesh(boxv_trimesh, smooth=False)
-
-direc_l = DirectionalLight(color=np.ones(3), intensity=1.0)
-spot_l = SpotLight(
-    color=np.ones(3),
-    intensity=10.0,
-    innerConeAngle=np.pi / 16,
-    outerConeAngle=np.pi / 6)
-point_l = PointLight(color=np.ones(3), intensity=10.0)
-
-camera = PerspectiveCamera(yfov=(np.pi / 3.0))
-cam_pose = np.array([[0.0, -np.sqrt(2) / 2,
-                      np.sqrt(2) / 2, 0.5], [1.0, 0.0, 0.0, 0.0],
-                     [0.0, np.sqrt(2) / 2,
-                      np.sqrt(2) / 2, 0.4], [0.0, 0.0, 0.0, 1.0]])
-
-scene = Scene(
-    bg_color=np.array([0.0, 0.0, 0.0]),
-    ambient_light=np.array([0.2, 0.2, 0.2, 1.0]))
-
-#==============================================================================
-# Adding objects to the scene
-#==============================================================================
-
-#------------------------------------------------------------------------------
-# By manually creating nodes
-#------------------------------------------------------------------------------
-boxv_node = Node(mesh=boxv_mesh, translation=np.array([0.0, 0.0, 0.0]))
-scene.add_node(boxv_node)
-
-#------------------------------------------------------------------------------
-# By using the add() utility function
-#------------------------------------------------------------------------------
-
-light_node = Node(light=point_l, translation=np.array([1, 1, 0]))
-scene.add_node(light_node)
-
-#==============================================================================
-# Using the viewer with a default camera
-#==============================================================================
-
-# v = Viewer(scene, shadows=True)
-
-#==============================================================================
-# Using the viewer with a pre-specified camera
-#==============================================================================
-cam_node = scene.add(camera, pose=cam_pose)
-v = Viewer(scene, central_node=boxv_node)
-
-#==============================================================================
-# Rendering offscreen from that camera
-#==============================================================================
-r = OffscreenRenderer(viewport_width=64, viewport_height=64)
-start_time = time.time()
-for k in range(1000):
-    boxv_node.translation = np.array([k / 100, 0.0, 0.0])
-    color, depth = r.render(scene)
-    # plt.imshow(color)
-    # plt.pause(1e-10)
-print(1000 / (time.time() - start_time))
-r.delete()
